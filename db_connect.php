dbadbadaed6ccb0ae0d794749262866c9db4c7fa
mariethomasson <marie.thomasson@gmail.com>
2014-12-01 14:38:44 +0100

    changed database link

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 
 <?php
-$db = new PDO('mysql:host=localhost;dbname=jensen', 'root', '');		//Databas kopplingen
+$db = new PDO('mysql:host=localhost;dbname=thomassons_jensen', 'thomassons_marie', 'marie');		//Databas kopplingen
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 		//Inställningar för error hantering
 $db->exec("SET NAMES 'utf8'");										//Teckenkod
 
