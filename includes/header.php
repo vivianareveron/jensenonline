ac05acefb65d99c78f3bdc4b323c496b35a202cf
mariethomasson <marie.thomasson@gmail.com>
2014-12-08 16:21:54 +0100

    changed the link to .php instead of .html

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <div class="navbar navbar-fixed-top">
   <div class="navbar-inner">
     <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span
-                    class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a class="brand" href="frontpage.html"><img src=img/Jensen.gif> </a>
+                    class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a class="brand" href="frontpage.php"><img src=img/Jensen.gif> </a>
       
         <div class="nav-collapse">
         <ul class="nav pull-right">
