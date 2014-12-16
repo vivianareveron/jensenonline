0c1cb977a07bdf525142538a4b475d3b81c1403d
mariethomasson <marie.thomasson@gmail.com>
2014-12-16 09:13:18 +0100

    moved the code for setting rights from dashbord to a separate file, x_rights_code.php

--- public/frontpage.php
+++ public/frontpage.php
@@ -14,19 +14,7 @@
 
         <h1>Välkommen till Jensen Online</h1>
 
-<?php
-$title = $_SESSION['title'];
 
-     if($title == 'Admin')  {
-        echo '<a href="one.php"><input type="button" value= "Click Me once!" header="one.php"></a><br><br>';
-        echo '<a href="two.php"><input type="button" value= "Click Me twice!"></a><br><br>';
-        echo '<a href="three.php"><input type="button" value= "Click Me three times!"></a><br><br>';
-    } elseif($title == 'Staff') {
-          echo '<a href="two.php"><input type="button" value= "Click Me twice!"></a><br><br>';
-         echo '<a href="three.php"><input type="button" value= "Click Me three times!"></a><br><br>';
-     } else
-         echo '<a href="three.php"><input type="button" value= "Click Me three times!"></a><br><br>';
-?>
         
     </div>
     
