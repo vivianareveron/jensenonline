673554cd6c75acbbf071725c3794e2c76eead7d7
mariethomasson <marie.thomasson@gmail.com>
2015-01-13 17:40:25 +0100

    made a signup page for courses

--- public/minakurser_signup.php
+++ public/minakurser_signup.php
@@ -14,144 +14,69 @@
     <div class="main">
     <div class="account-container">
         <div class="content clearfix">
-        <h1>Lägg till kurser</h1>
-    <p>Lägg till kurser i Jensen Online. </p>
-    <i>Fält markerade med en <span class="error">*</span> är obligatoriska.</i><br /><br />
+        <h1>Lägg till kurs</h1>
+    <p>Lägg till nya kurser i Jensen Online. </p>
+    <i>Fält markerade med en <span class="error">*</span> är obligatoriska.</i>
 
 <?php
-    $title = $class = $firstname = $lastname = $address = $postnumber = $postaddress = $email = $phone = $mobile = $username = $password = $re_password = "" ;
-	$titleErr = $classErr = $firstErr = $lastErr = $emailErr = $userErr = $passErr = $rePassErr = "";
+    $class = $status = $course = $startdate = $enddate = $rating = "" ;
+	$classErr = $statusErr = $courseErr = $startErr = $endErr = $ratingErr = "";
 $msg = "";
 
 //Om användaren trycker på "Sign up"-knappen
 if(isset($_POST["submit"])){								
 	
-		$title        = trim($_POST["title"]);
         $class        = trim($_POST["class"]);
-        $firstname    = trim($_POST["firstname"]);	
-		$lastname     = trim($_POST["lastname"]);
-		$address      = trim($_POST["address"]);	
-		$postnumber   = trim($_POST["postnumber"]);
-        $postaddress  = trim($_POST["postaddress"]);	
-		$email 	      = trim($_POST["email"]);
-		$phone        = trim($_POST["phone"]);	
-		$mobile       = trim($_POST["mobile"]);
-        $username    = trim($_POST["username"]);
-		$password     = trim($_POST["password"]);
-        $re_password  = trim($_POST["re_password"]);
+        $status       = trim($_POST["status"]);
+        $course       = trim($_POST["course"]);	
+		$startdate    = trim($_POST["startdate"]);
+		$enddate      = trim($_POST["enddate"]);	
+		$rating       = trim($_POST["rating"]);
 	
-		if (empty($_POST["title"])) {
-			$titleErr = "Title is required";
-		}
-        if (empty($_POST["class"])) {
+		if (empty($_POST["class"])) {
 			$classErr = "Class is required";
 		}
-        if (!preg_match("/^[A-Za-z åäöÅÄÖ ´`-]*$/",$firstname)) {
-			$firstErr = "Only letters are allowed"; 
+        if (empty($_POST["status"])) {
+			$statusErr = "Status is required";
 		}
-        if (empty($_POST["firstname"])) {
-			$firstErr = "Firstname is required";
+        if (empty($_POST["course"])) {
+			$courseErr = "Course is required";
 		}
-        if (!preg_match("/^[A-Za-z åäöÅÄÖ ´`-]*$/",$lastname)) {
-			$lastErr = "Only letters are allowed"; 
+        if (!preg_match("/^[0-9 -]*$/",$startdate)) {
+			$startErr = "Only yyyy-mm-dd format is allowed"; 
 		}
-        if (empty($_POST["lastname"])) {
-			$lastErr = "Lastname is required";
-		}
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			$emailErr = "Invalid email format"; 
-		}
-	    if (empty($_POST["email"])) {
-			$emailErr = "Email is required";
+        if (empty($_POST["startdate"])) {
+			$startErr = "Startdate is required";
 	    }
-        if (!preg_match("/^[A-Za-z0-9]*$/",$username)) {
-			$userErr = "Only letters and numbers are allowed"; 
+        if (!preg_match("/^[0-9 -]*$/",$enddate)) {
+			$endErr = "Only yyyy-mm-dd format is allowed"; 
 		}
-        if (empty($_POST["username"])) {
-			$userErr = "Username is required";
+        if (empty($_POST["enddate"])) {
+			$endErr = "Enddate is required";
 	    }
-        if (!preg_match("/^[A-Za-z0-9]*$/",$password)) {
-			$passErr = "Only letters and numbers are allowed"; 
+        if (!preg_match("/^[0-9]*$/",$rating)) {
+			$ratingErr = "Only numbers is allowed"; 
 		}
-	    if (empty($_POST["password"])) {
-			$passErr = "Password is required";
-	    }
-        if (empty($_POST["re_password"])) {
-			$rePassErr = "Re-entered password is required";
+        if (empty($_POST["rating"])) {
+			$ratingErr = "Rating is required";
 	    } 
-        if($re_password!=$password) {
-            $rePassErr = "The re-entered password don't match";
-        }
     
-        try{
-            require_once("../includes/db_connect.php");
-            
-
-            $query = "SELECT * ";
-            $query .= "FROM users ";
-            $query .= "WHERE username = :username "; 
-
-            $ps = $db->prepare($query); 
-            $result = $ps->execute(
-                array(
-                    'username'=>$username 
-                ));
-            $result = $ps->fetch(PDO::FETCH_ASSOC);
-
-                if($username){	
-                    if ($result ['username']== $username) {
-
-                        $_SESSION["username"] = $result['username'];
-
-                    $userErr = "Username already exist. Please create a new username.<br /><br />";
-
-                    } 
-                } 
-                if($email){	
-                    if ($result ['email']== $email) {
-
-                        $_SESSION["email"] = $result['email'];
-
-                    $emailErr = "Email already exist. Please create a new email.<br /><br />";
-
-                    } 
-                } 
-
-        } catch(Exception $exception) {
-            echo "Query failed, see error message below: <br /><br />";
-            echo $exception. "<br /> <br />";
-        }
-
-	    
-        if(empty($titleErr) && empty($classErr) && empty($firstErr) && empty($lastErr) && empty($emailErr) && empty($userErr) && empty($passErr) && empty($rePassErr)){
+        if(empty($classErr) && empty($statusErr) && empty($courseErr) && empty($startErr) && empty($endErr) && empty($ratingErr)){
                  
 		try{
-        require_once("../includes/db_connect.php");
-            //här gör vi hashade passwords
-            $options = [
-			'cost' => 12,			
-            ];
+        require_once("../includes/db_connect.php"); 
 
-            $hashedPass = password_hash($password, PASSWORD_BCRYPT, $options); 
-            //slut hash 
-
-            $query = "INSERT INTO users (title, class, firstname, lastname, address, postnumber, postaddress, email, phone, mobile, username, password) ";
-            $query .= "VALUES (:title, :class, :firstname, :lastname, :address, :postnumber, :postaddress, :email, :phone, :mobile, :username, :password) ";
+            $query = "INSERT INTO courses (class, status, course, startdate, enddate, rating) ";
+            $query .= "VALUES (:class, :status, :course, :startdate, :enddate, :rating) ";
 
             $ps = $db->prepare($query); //prepared statement
             $result = $ps->execute(array(
-                'title'=>$title,
                 'class'=>$class,
-                'firstname'=>$firstname,
-                'lastname'=>$lastname, 
-                'address'=>$address,
-                'postnumber'=>$postnumber,
-                'postaddress'=>$postaddress,
-                'email'=>$email, 
-                'phone'=>$phone,
-                'mobile'=>$mobile,
-                'username'=>$username,
-                'password'=>$hashedPass,//tidigare $password
+                'status'=>$status,
+                'course'=>$course, 
+                'startdate'=>$startdate,
+                'enddate'=>$enddate,
+                'rating'=>$rating
             ));
 
                 if ($result) {
@@ -178,19 +103,8 @@ if(isset($_POST["submit"])){
 ?>
             
 <!-- Formulär --> 
-<form action="signup.php" method="POST" >
+<form action="minakurser_signup.php" method="POST" >
         <table>
-            <tr>
-                <th><label for="title">Titel: </label></th>
-                <td>
-                <select name="title" id="title" >
-                    <option value="">-- Välj --</option>
-                    <option value="Student">Elev</option>
-                    <option value="Staff">Lärare</option>
-                    <option value="Admin">Admin</option>
-                </td>
-                <td><span class="error"> * <?php echo $titleErr; ?></span></td>
-            </tr>
             <tr>
                 <th><label for="class">Klass: </label></th>
                 <td>
@@ -205,49 +119,32 @@ if(isset($_POST["submit"])){
                 <td><span class="error">* <?php echo $classErr; ?></span></td>
             </tr>
             <tr>
-                <td>Förnamn </td>
-                <td><input type="text" name="firstname" value="<?php echo $firstname; ?>"/><span class="error"> * <?php echo $firstErr; ?></span></td>
+                <th><label for="status">Status: </label></th>
+                <td>
+                <select name="status" id="status" >
+                    <option value="">-- Välj --</option>
+                    <option value="Kommande">Kommande</option>
+                    <option value="Pågående">Pågående</option>
+                    <option value="Avslutad">Avslutad</option>
+                </td>
+                <td><span class="error"> * <?php echo $statusErr; ?></span></td>
             </tr>
             <tr>
-                <td>Efternamn </td>
-                <td><input type="text" name="lastname" value="<?php echo $lastname; ?>"/><span class="error"> * <?php echo $lastErr; ?></span></td>
+                <td>Kurs </td>
+                <td><input type="text" name="course" value="<?php echo $course; ?>"/><span class="error"> * <?php echo $courseErr; ?></span></td>
             </tr>
             <tr>
-                <td>Adress </td>
-                <td><input type="text" name="address" value="<?php echo $address; ?>"/></td>
+                <td>Startdatum </td>
+                <td><input type="text" name="startdate" value="<?php echo $startdate; ?>"/><span class="error"> * <?php echo $startErr; ?></span></td>
             </tr>
             <tr>
-                <td>Postnummer </td>
-                <td><input type="text" name="postnumber" value="<?php echo $postnumber; ?>" /></td>
+                <td>Slutdatum </td>
+                <td><input type="text" name="enddate" value="<?php echo $enddate; ?>" /><span class="error"> * <?php echo $endErr; ?></span></td>
             </tr>
             <tr>
-                <td>Postadress </td>
-                <td><input type="text" name="postaddress" value="<?php echo $postaddress; ?>" /></td>
+                <td>Poäng </td>
+                <td><input type="text" name="rating" value="<?php echo $rating; ?>" /><span class="error"> * <?php echo $ratingErr; ?></span></td>
             </tr>
-            <tr>
-                <td>E-post </td>
-                <td><input type="email" name="email" value="<?php echo $email; ?>"/><span class="error"> * <?php echo $emailErr; ?></span></td>
-            </tr>
-            <tr>
-                <td>Telefon </td>
-                <td><input type="text" name="phone" value="<?php echo $phone; ?>"/></td>
-            </tr>
-            <tr>
-                <td>Mobil </td>
-                <td><input type="text" name="mobile" value="<?php echo $mobile; ?>"/></td>
-            </tr>
-            <tr>
-                <td>Username </td>
-                <td><input type="text" name="username" value="<?php echo $username; ?>"/><span class="error"> * <?php echo $userErr; ?></span></td>
-            </tr>
-            <tr>
-                <td>Password </td>
-                <td><input type="password" name="password" value="<?php echo $password; ?>" /><span class="error"> * <?php echo $passErr; ?></span></td>
-            </tr>
-            <tr>
-			<td>Re-password:</td>
-			<td><input type="password" name="re_password" /><span class="error"> * <?php echo $rePassErr; ?></span></td>
-		</tr>
 
 <?php echo $_SESSION['msg']; ?><br /><br />
             
--- public/minklass_signup.php
+++ public/minklass_signup.php
@@ -14,8 +14,8 @@
     <div class="main">
     <div class="account-container">
         <div class="content clearfix">
-        <h1>Lägg till användare</h1>
-    <p>Lägg till nya elever, lärare och övrig personal i Jensen Online. </p>
+        <h1>Lägg till kurser</h1>
+    <p>Lägg till kurser i Jensen Online. </p>
     <i>Fält markerade med en <span class="error">*</span> är obligatoriska.</i><br /><br />
 
 <?php
@@ -178,7 +178,7 @@ if(isset($_POST["submit"])){
 ?>
             
 <!-- Formulär --> 
-<form action="minklass_signup.php" method="POST" >
+<form action="signup.php" method="POST" >
         <table>
             <tr>
                 <th><label for="title">Titel: </label></th>
