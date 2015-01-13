951ced19b023789d9be65cf8fa734604b9703c49
mariethomasson <marie.thomasson@gmail.com>
2015-01-13 14:42:43 +0100

    fixed spelling errors

--- public/layout/shortcuts.php
+++ public/layout/shortcuts.php
@@ -8,19 +8,19 @@
            
                 <div class="shortcuts">
                     <div> 
-                        <a href="minakurser.php" class="shortcut"><i class="shortcut-icon icon-list-alt"></i><span class="shortcut-label">Mina Kurs</span> </a>
+                        <a href="minakurser.php" class="shortcut"><i class="shortcut-icon icon-list-alt"></i><span class="shortcut-label">Mina kurser</span> </a>
                     </div>
                     <div>    
                     <a href="meddelanden.php" class="shortcut"><i class="shortcut-icon icon-envelope"></i><span class="shortcut-label">Meddelanden</span> </a>
                     </div>
                     <div>
-                    <a href="minklass.php" class="shortcut"><i class="shortcut-icon icon-group"></i> <span class="shortcut-label">Mina Klass</span> </a>
-                    </div>
-                    <div>
-                    <a href="franvaro.php" class="shortcut"><i class="shortcut-icon icon-ok-sign "></i><span class="shortcut-label">Anmäl frånvaro</span> </a>
+                    <a href="minklass.php" class="shortcut"><i class="shortcut-icon icon-group"></i> <span class="shortcut-label">Min klass</span> </a>
                     </div>
                     <div>
                     <a href="chat.php" class="shortcut"><i class="shortcut-icon icon-comment"></i><span class="shortcut-label">Chat</span> </a>
+                    </div>
+                    <div>
+                    <a href="franvaro.php" class="shortcut"><i class="shortcut-icon icon-ok-sign "></i><span class="shortcut-label">Frånvaro</span> </a>
                     </div>
                  
             </div>
